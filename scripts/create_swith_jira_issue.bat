e392dbe605294ab9b60b176bd5125b7231eb4f9f
bublade <alexthvb@gmail.com>
2025-09-19 10:00:26 +0200

    removed pmr and initials from default format

--- scripts/create_swith_jira_issue.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-git switch -q "feature/%1" || git switch -c "feature/%1"
-
--- scripts/jira_branches.bat
+++ scripts/jira_branches.bat
@@ -1,7 +1,7 @@
 @echo off
 curl.exe -s -H ^
   "Authorization: Bearer %JR_CLI_AT%" ^
-  "%JR_CLI_HOST%/rest/api/2/search?jql=assignee+in+(currentUser())+and+resolution+is+empty&fields=customfield_10008,summary" ^
-  | jq ".issues | map ({key, \"summary\": .fields.summary, \"pmr\": .fields.customfield_10008.[0]})" ^
-  | jq -r ".[] | [.pmr, .key, \"AtH\", .summary] | map(if . == null then \"00.000.000\" else . end) | join(\"_\") | gsub(\"[\\s()']\"; \"_\")"
+  "%JR_CLI_HOST%/rest/api/2/search?jql=assignee+in+(currentUser())+and+resolution+is+empty&fields=summary" ^
+  | jq ".issues | map ({key, \"summary\": .fields.summary})" ^
+  | jq -r ".[] | [.key, .summary] | join(\"-\") | gsub(\"\\s[-^&]\\s\"; \"-\") | gsub(\"[\\/\\s]\"; \"-\") | gsub(\"[()']\"; \"\")"
 
