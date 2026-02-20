52c11dc9ecc03c67d18b90288ad79200472199f7
bublade <alexthvb@gmail.com>
2026-02-20 11:07:46 +0100

    jira_branches improvements

--- scripts/jira_branches.ps1
+++ scripts/jira_branches.ps1
@@ -3,9 +3,12 @@
 $JR_CLI_AT=$env:JR_CLI_AT
 $JR_CLI_HOST=$env:JR_CLI_HOST
 
+$jql="assignee = currentUser() AND resolution = Unresolved order by updated DESC"
+$jql_encoded=[System.Web.HttpUtility]::UrlEncode($jql)
+
 curl.exe -s `
-  "$JR_CLI_HOST/rest/api/2/search?jql=assignee+in+(currentUser())+and+resolution+is+empty&fields=summary" `
+  "$JR_CLI_HOST/rest/api/2/search?jql=$jql_encoded&fields=summary" `
     -H "Authorization: Bearer $JR_CLI_AT" ` 
   | jq '.issues | map ({key, "summary": .fields.summary})' `
-  | jq -r '.[] | [.key, .summary] | join("-") | gsub("\\s[-^&]\\s"; "-") | gsub("[\\/\\s]"; "-") | gsub("[()'']"; "")'
+  | jq -r '.[] | [.key, .summary] | join("-") | gsub("\\s[-^&]\\s"; "-") | gsub("[\\/\\s]"; "-") | gsub("[()'']"; "") | gsub("\\.$"; "")'
 
