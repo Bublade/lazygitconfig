9281dce01a96d2a47c6cfef06916c93a4e1cf0f1
bublade <alexthvb@gmail.com>
2026-02-20 16:37:14 +0100

    check for issue type

--- scripts/jira_branches.ps1
+++ scripts/jira_branches.ps1
@@ -7,8 +7,12 @@ $jql="assignee = currentUser() AND resolution = Unresolved order by updated DESC
 $jql_encoded=[System.Web.HttpUtility]::UrlEncode($jql)
 
 curl.exe -s `
-  "$JR_CLI_HOST/rest/api/2/search?jql=$jql_encoded&fields=summary" `
+  "$JR_CLI_HOST/rest/api/2/search?jql=$jql_encoded&fields=summary,issuetype" `
     -H "Authorization: Bearer $JR_CLI_AT" ` 
-  | jq '.issues | map ({key, "summary": .fields.summary})' `
-  | jq -r '.[] | [.key, .summary] | join("-") | gsub("\\s[-^&]\\s"; "-") | gsub("[\\/\\s]"; "-") | gsub("[()'']"; "") | gsub("\\.$"; "")'
+  | jq '.issues | map ({key, "summary": .fields.summary, "issuetype": .fields.issuetype})' `
+  | jq -r '.[]
+          | [
+                (if .issuetype.name == "Bug" then "bugfix" else if .issuetype == "New Feature" then "feature" else "feature" end end),
+                ([.key, .summary] | join("-") | gsub("\\s[-^&]\\s"; "-") | gsub("[\\/\\s]"; "-") | gsub("[()'']"; "") | gsub("\\.$"; ""))
+            ] | join("/")'
 
